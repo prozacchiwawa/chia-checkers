499681d898c49e338ee8097698ebdcee4a3a2799
arty <art.yerkes@gmail.com>
2021-08-17 07:27:36 -0700

    More improvement

--- tests/test_checkers.py
+++ tests/test_checkers.py
@@ -20,9 +20,6 @@ from cdv.util.load_clvm import load_clvm
 from cdv.test import setup as setup_test
 from cdv.test import ContractWrapper, CoinWrapper
 
-LAUNCHER_MOD = load_clvm("singleton_launcher.clvm", "chia.wallet.puzzles")
-SINGLETON_MOD = load_clvm("singleton_top_layer.clvm", "chia.wallet.puzzles")
-
 GAME_MOJO = 1 # 1 mojo
 INITIAL_BOARD = SExp.to([1, 0, 0xa040a040a040a040, 0x205020502050205])
 
@@ -38,6 +35,38 @@ def make_move_sexp(fromX,fromY,toX,toY):
 #
 # It incorporates a currying of fixed parameters and the board, leaving just
 # a move as a usable parameter for the solution.
+#
+# Theory of operation:
+#
+# This contract creates a playable game of checkers which carries some attributes
+# of the game in its solution so that they can be picked out by another
+# participant, including the identity of the coin that launched it, which must
+# be restated to interact with it.
+#
+# The game is a function that accepts 3 arguments, for a normal move:
+#
+# (() (move) (("launcher" . launcher-coin) ("board" board)))
+#
+# The game uses a board state like this:
+#
+# (black-to-move king-mask red-mask black-mask)
+#
+# And it is curried in at each stage.  The copy in the third parameter, which
+# as I understand things is intended to be an alist containing data we want to
+# communicate to other users should contain the identity of the original parent
+# coin, "launcher", which will be verified and the board state "board", which is
+# also verified before any operation.  The next move is emitted with an AGG_SIG_ME
+# for the player who's turn it was, so that turn order is enforced.
+#
+# A move is a number as in make_move_sexp.
+#
+# When no moves can be taken by the next player, the winning player may win the
+# game by passing () for move and the chia is given to that player.
+#
+# The first argument may be given as "simulate" in which case, the contract can
+# be asked to give its conception of the next puzzle hash and the board state
+# that goes with it, given a move.  This is used in a rudimentary way for driver
+# code to be able to ask the contract what will happen when a move is requested.
 #
 class TestCheckers:
     @pytest.fixture(scope="function")
